36ed460a784c2a30c9c366eb3ece9f6e49f87136
mastercodercat <mastercoderoy64@gmail.com>
2016-09-22 15:57:51 +0200

    Host and service page time period selet done

--- frontend/client/containers/Host/Host.js
+++ frontend/client/containers/Host/Host.js
@@ -5,29 +5,35 @@ import Immutable from 'immutable'
 class Host extends Component {
 
   state = {
-    events: Immutable.fromJS({})
+    period: 1,
   }
 
-  componentWillMount() {
+  calculateData = () => {
     const { events } = this.props
     const currentHost = this.props.params.host
+    const { period } = this.state
+    const date = new Date()
+    date.setHours(date.getHours() - period)
     let sortedEvents = Immutable.fromJS({})
     events.map(event => {
       const host = event.get('remote_host')
       if (host == currentHost) {
-        const service = event.get('service')
-        const eventsOfService = sortedEvents.get(service) ? sortedEvents.get(service) : []
-        eventsOfService.push(event)
-        sortedEvents = sortedEvents.set(service, eventsOfService)
+        const eventDate = new Date(event.get('datetime'))
+        if (eventDate >= date) {
+          const service = event.get('service')
+          const eventsOfService = sortedEvents.get(service) ? sortedEvents.get(service) : []
+          eventsOfService.push(event)
+          sortedEvents = sortedEvents.set(service, eventsOfService)
+        }
       }
     })
-    this.setState({
-      events: sortedEvents
-    })
+    return sortedEvents
   }
 
   render() {
-    const { events } = this.state
+    const { period } = this.state
+    const events = this.calculateData()
+    const currentHost = this.props.params.host
 
     const data = []
     events.map((eventsOfService, key) => {
@@ -42,7 +48,7 @@ class Host extends Component {
         type: 'pie'
       },
       title: {
-        text: 'Top Attack Hosts'
+        text: 'Activity from host: ' + currentHost
       },
       tooltip: {
         pointFormat: 'Attacks: <b>{point.y}</b>'
@@ -61,7 +67,34 @@ class Host extends Component {
     return (
       <div>
         <h3 className="title">Host</h3>
+        <div className="m-t-3 m-b-2">
+          <label>Period:</label>
+          <select
+            className="form-control"
+            style={{ width: 300 }}
+            value={period}
+            onChange={e => this.setState({ period: e.currentTarget.value })}>
+            <option value={1}>Last one hour</option>
+            <option value={24}>Last one day</option>
+            <option value={168}>Last one week</option>
+            <option value={168 * 30}>Last 30 days</option>
+            <option value={168 * 180}>Last 6 months</option>
+          </select>
+        </div>
         <ReactHighcharts config={config} />
+        <div className="m-t-3">
+          <div>
+            Lookup tools:
+            <a className="lookup-link" href="" target="_blank">DShield</a>
+            <a className="lookup-link" href="" target="_blank">Firyx</a>
+            <a className="lookup-link" href="" target="_blank">Twitter</a>
+            <a className="lookup-link" href="" target="_blank">Google</a>
+            <a className="lookup-link" href="" target="_blank">Virus Total</a>
+            <a className="lookup-link" href="" target="_blank">Spamhaus</a>
+            <a className="lookup-link" href="" target="_blank">Spamcop</a>
+            <a className="lookup-link" href="" target="_blank">Senderbase</a>
+          </div>
+        </div>
       </div>
     )
   }
--- frontend/client/containers/Service/Service.js
+++ frontend/client/containers/Service/Service.js
@@ -5,29 +5,36 @@ import Immutable from 'immutable'
 class Service extends Component {
 
   state = {
-    events: Immutable.fromJS({})
+    period: 1,
   }
 
-  componentWillMount() {
+  calculateData = () => {
     const { events } = this.props
     const currentService = this.props.params.service
+    const { period } = this.state
+    const currentHost = this.props.params.host
+    const date = new Date()
+    date.setHours(date.getHours() - period)
     let sortedEvents = Immutable.fromJS({})
     events.map(event => {
       const service = event.get('service')
       if (service == currentService) {
-        const host = event.get('remote_host')
-        const eventsOfHost = sortedEvents.get(host) ? sortedEvents.get(host) : []
-        eventsOfHost.push(event)
-        sortedEvents = sortedEvents.set(host, eventsOfHost)
+        const eventDate = new Date(event.get('datetime'))
+        if (eventDate >= date) {
+          const host = event.get('remote_host')
+          const eventsOfHost = sortedEvents.get(host) ? sortedEvents.get(host) : []
+          eventsOfHost.push(event)
+          sortedEvents = sortedEvents.set(host, eventsOfHost)
+        }
       }
     })
-    this.setState({
-      events: sortedEvents
-    })
+    return sortedEvents
   }
 
   render() {
-    const { events } = this.state
+    const events = this.calculateData()
+    const { period } = this.state
+    const currentService = this.props.params.service
 
     const data = []
     events.map((eventsOfHost, key) => {
@@ -42,7 +49,7 @@ class Service extends Component {
         type: 'pie'
       },
       title: {
-        text: 'Top Attack Service'
+        text: 'Activity on the service: ' + currentService
       },
       tooltip: {
         pointFormat: 'Attacks: <b>{point.y}</b>'
@@ -61,6 +68,20 @@ class Service extends Component {
     return (
       <div>
         <h3 className="title">Service</h3>
+        <div className="m-t-3 m-b-2">
+          <label>Period:</label>
+          <select
+            className="form-control"
+            style={{ width: 300 }}
+            value={period}
+            onChange={e => this.setState({ period: e.currentTarget.value })}>
+            <option value={1}>Last one hour</option>
+            <option value={24}>Last one day</option>
+            <option value={168}>Last one week</option>
+            <option value={168 * 30}>Last 30 days</option>
+            <option value={168 * 180}>Last 6 months</option>
+          </select>
+        </div>
         <ReactHighcharts config={config} />
       </div>
     )
